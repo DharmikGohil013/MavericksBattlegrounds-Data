3e771f6f17d94af01d66f7690df39d73051e2341
DharmikGohil013 <dharmikgohil395003@gmail.com>
2025-05-27 21:50:50 +0530

    add

--- api/users.js
+++ api/users.js
@@ -1,7 +1,5 @@
-// api/users.js
-const mongoose = require('mongoose');
-const User = require('../models/User');
 const connectDB = require('../utils/db');
+const User = require('../models/User');
 
 module.exports = async (req, res) => {
   await connectDB();
@@ -9,22 +7,15 @@ module.exports = async (req, res) => {
   if (req.method === 'POST') {
     const { name } = req.body;
 
-    if (!name) {
-      return res.status(400).json({ message: 'Name is required' });
-    }
+    if (!name) return res.status(400).json({ message: 'Name is required' });
 
-    try {
-      const exists = await User.findOne({ name });
-      if (exists) return res.status(409).json({ message: 'Name already exists' });
+    const exists = await User.findOne({ name });
+    if (exists) return res.status(409).json({ message: 'Name already exists' });
 
-      const newUser = new User({ name });
-      await newUser.save();
-      return res.status(201).json({ message: 'User created', user: newUser });
-    } catch (err) {
-      return res.status(500).json({ message: 'Server error', error: err.message });
-    }
-  } else {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+    const newUser = new User({ name });
+    await newUser.save();
+    return res.status(201).json({ message: 'User created successfully', user: newUser });
   }
+
+  res.status(405).json({ message: 'Method not allowed' });
 };
--- utils/db.js
+++ utils/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   if (mongoose.connections[0].readyState) return;
+
   await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
